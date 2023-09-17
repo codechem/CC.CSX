90f3fbacc23bc171d12e445e2f44c2f2f5c2103a
halicea <costa@codechem.com>
2023-09-17 11:30:55 +0200

    [DEV] removing the implementation for placeholders. Need to think of an effective way to do it

--- src/CC.CSX/HtmlElementExtensions.cs
+++ src/CC.CSX/HtmlElementExtensions.cs
@@ -1,25 +1,26 @@
 namespace CC.CSX;
-public static class HtmlElementExtensions
-{
-    public static HtmlNode WithParent(HtmlNode child, HtmlNode parent, string placeholderId)
-        => parent.Placeholders[placeholderId].Add(child);
-}
-
-public class HtmlPlacehoder : HtmlItem
-{
-    public List<HtmlNode> Children{get; private set;}= new();
-    public HtmlPlacehoder(string name) : base(name)
-    {
-    }
-
-    public HtmlPlacehoder(string name, string? value) : base(name, value)
-    {
-    }
-
-    public override string ToString(int indent = 0)
-    {
-        if(Children.Any()){
-            return new Fragment(children);
-        }
-    }
-}
+// TODO: WIP
+// public static class HtmlElementExtensions
+// {
+//     public static HtmlNode WithParent(HtmlNode child, HtmlNode parent, string placeholderId)
+//         => parent.Placeholders[placeholderId].Add(child);
+// }
+//
+// public class HtmlPlacehoder : HtmlItem
+// {
+//     public List<HtmlNode> Children{get; private set;}= new();
+//     public HtmlPlacehoder() : base("#placeholder")
+//     {
+//     }
+//
+//     public HtmlPlacehoder(string name, string? value) : base(name, value)
+//     {
+//     }
+//
+//     public override string ToString(int indent = 0)
+//     {
+//         if(Children.Any()){
+//             return new Fragment(children);
+//         }
+//     }
+// }
