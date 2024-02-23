c1b3cd5b72cdc518d7e2714fcc74d092c92252e8
halicea <costa@codechem.com>
2024-02-23 08:33:03 +0100

    [OPS] main | just ensuring if build and tests are ok

--- eng/github-build-all.sh
+++ eng/github-build-all.sh
@@ -1,8 +1,2 @@
-source eng/load_env.sh && \
-dotnet build  src/CC.CSX/CC.CSX.csproj -c Release && \
+dotnet build  -c Release
 
-dotnet add src/CC.CSX.Web/CC.CSX.Web.csproj package CC.CSX --version $VERSION --source $NUGET_SOURCE && \
-dotnet build src/CC.CSX.Web/CC.CSX.Web.csproj -c Release && \
-
-dotnet add src/CC.CSX.Htmx/CC.CSX.Htmx.csproj package CC.CSX --version $VERSION --source $NUGET_SOURCE && \
-dotnet build src/CC.CSX.Htmx/CC.CSX.Htmx.csproj -c Release
